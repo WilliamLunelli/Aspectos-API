27d7a6233d69fc8219e38c755e2a1e530df08511
williamlunelli <williamlunelli07@gmail.com>
2024-09-30 09:35:55 -0300

    atualização

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -5,3 +5,10 @@ export const mainRouter = Router();
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.post("/produtos", (req, res) => {
+    //receber dados
+    //processar dados
+
+    
+} )
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
-import express, { urlencoded } from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-import { mainRouter } from './routes/main';
+import express from 'express';
 import helmet from 'helmet';
+import router from './routes';
 
 const server = express();
+
 server.use(helmet());
-server.use(cors());
-server.use(urlencoded({ extended: true }));
-server.disable('x-powered-by');
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
-server.use(mainRouter);
+server.use('/', router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${port}`);
-})
+server.listen(3000, () => {
+    console.log('Servidor rodando: http://localhost:3000/');
+});
