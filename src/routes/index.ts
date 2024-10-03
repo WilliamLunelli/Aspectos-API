026f0467510d92b00e8cf1607c6ae9d3aee13d04
williamlunelli <williamlunelli07@gmail.com>
2024-10-02 22:08:46 -0300

    Working with Middlewares

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createContactController, deleteContactController, getContactsController } from '../Controllers/contactController';
+import { privateRequest } from '../Middlewares/auth';
 
 const router = express.Router();
 
-router.post('/contato', createContactController);
+router.post('/contato',privateRequest, createContactController);
 
 router.get('/contatos', getContactsController);
 
-router.delete('/contato', deleteContactController);
+router.delete('/contato', privateRequest, deleteContactController);
 
 export default router;
